badd4d114791c083583b7373c33c2acfb043f2a9
Pablo-deck <pablobaldeon02@gmail.com>
2025-05-25 15:25:04 -0500

    Add files via upload

---
+++ Proyecto/src/gui/Administrar.java
@@ -0,0 +1,126 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Administrar extends JDialog implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+	private JTextField txtProducto;
+	private JTextArea txtS;
+	private JButton btnAnadir;
+	private JButton btnEliminar;
+	private JButton btnCerrar;
+	private JButton btnBuscar;
+	private JTextField txtPrecio;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			Administrar dialog = new Administrar();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public Administrar() {
+		setModal(true);
+		setBounds(100, 100, 377, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nombre Producto:");
+		lblNewLabel.setBounds(10, 11, 93, 14);
+		contentPanel.add(lblNewLabel);
+		
+		txtProducto = new JTextField();
+		txtProducto.setBounds(113, 8, 137, 20);
+		contentPanel.add(txtProducto);
+		txtProducto.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 61, 239, 189);
+		contentPanel.add(scrollPane);
+		{
+			txtS = new JTextArea();
+			scrollPane.setViewportView(txtS);
+		}
+		{
+			btnAnadir = new JButton("AÑADIR");
+			btnAnadir.addActionListener(this);
+			btnAnadir.setBounds(260, 7, 89, 23);
+			contentPanel.add(btnAnadir);
+		}
+		{
+			btnEliminar = new JButton("ELIMINAR");
+			btnEliminar.addActionListener(this);
+			btnEliminar.setBounds(259, 38, 89, 23);
+			contentPanel.add(btnEliminar);
+		}
+		{
+			btnCerrar = new JButton("CERRAR");
+			btnCerrar.addActionListener(this);
+			btnCerrar.setBounds(259, 227, 89, 23);
+			contentPanel.add(btnCerrar);
+		}
+		{
+			btnBuscar = new JButton("BUSCAR");
+			btnBuscar.addActionListener(this);
+			btnBuscar.setBounds(259, 72, 89, 23);
+			contentPanel.add(btnBuscar);
+		}
+		
+		txtPrecio = new JTextField();
+		txtPrecio.setColumns(10);
+		txtPrecio.setBounds(113, 33, 137, 20);
+		contentPanel.add(txtPrecio);
+		
+		JLabel lblNewLabel_1 = new JLabel("Precio Unitario:");
+		lblNewLabel_1.setBounds(10, 36, 93, 14);
+		contentPanel.add(lblNewLabel_1);
+	}
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnCerrar) {
+			do_btnCerrar_actionPerformed(e);
+		}
+		if (e.getSource() == btnBuscar) {
+			do_btnBuscar_actionPerformed(e);
+		}
+		if (e.getSource() == btnEliminar) {
+			do_btnEliminar_actionPerformed(e);
+		}
+		if (e.getSource() == btnAnadir) {
+			do_btnAnadir_actionPerformed(e);
+		}
+	}
+	protected void do_btnAnadir_actionPerformed(ActionEvent e) {
+	}
+	protected void do_btnEliminar_actionPerformed(ActionEvent e) {
+	}
+	protected void do_btnBuscar_actionPerformed(ActionEvent e) {
+	}
+	protected void do_btnCerrar_actionPerformed(ActionEvent e) {
+		dispose();
+	}
+}
---
+++ Proyecto/src/gui/Boleta.java
@@ -0,0 +1,333 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JSpinner;
+import javax.swing.JSpinner.DefaultEditor;
+import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import clases.ArregloProducto;
+import clases.Producto;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+
+public class Boleta extends JDialog implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+	private JComboBox cbProducto;
+	private JLabel lblNewLabel_1;
+	private JSpinner sCantidad;
+	private JLabel lblPrecio;
+	private JTextField txtPrecio;
+	private JButton btnAgregar;
+	private JButton btnGenerar;
+	ArregloProducto ap1 = new ArregloProducto();
+	private JButton btnCerrar;
+	private JButton btnModificar;
+	private JScrollPane scrollPane;
+	private JTable tS;
+	private JButton btnQuitar;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			Boleta dialog = new Boleta();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public Boleta() {
+		setTitle("GENERAR BOLETA");
+		setModal(true);
+		setBounds(100, 100, 522, 272);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JLabel lblNewLabel = new JLabel("PRODUCTO");
+			lblNewLabel.setBounds(10, 11, 81, 14);
+			contentPanel.add(lblNewLabel);
+		}
+		{
+			ArregloProducto pro = new ArregloProducto();
+			cbProducto = new JComboBox();
+			cbProducto.addActionListener(this);
+			DefaultComboBoxModel<String> modelo = new DefaultComboBoxModel<>();
+			for (int i = 0; i < pro.Tamano(); i++) {
+				modelo.addElement(pro.Obtener(i).getProducto());
+			}
+			cbProducto.setModel(modelo);
+			
+			cbProducto.setBounds(81, 7, 138, 22);
+			contentPanel.add(cbProducto);
+		}
+		{
+			lblNewLabel_1 = new JLabel("CANTIDAD");
+			lblNewLabel_1.setBounds(229, 11, 81, 14);
+			contentPanel.add(lblNewLabel_1);
+		}
+		{
+			SpinnerNumberModel model = new SpinnerNumberModel(1,1,100,1 );
+			sCantidad = new JSpinner(model);
+			DefaultEditor editor=(DefaultEditor) sCantidad.getEditor();
+			JTextField textField=editor.getTextField();
+			textField.setEditable(false);
+			sCantidad.addChangeListener(new ChangeListener() {
+				public void stateChanged(ChangeEvent e) {
+				CalcularPrecio();
+				}
+			});
+			sCantidad.setBounds(293, 8, 36, 20);
+			contentPanel.add(sCantidad);
+		}
+		{
+			lblPrecio = new JLabel("PRECIO");
+			lblPrecio.setBounds(357, 11, 81, 14);
+			contentPanel.add(lblPrecio);
+		}
+		{
+			txtPrecio = new JTextField();
+			txtPrecio.setText("0.20");
+			txtPrecio.setEditable(false);
+			txtPrecio.setEnabled(false);
+			txtPrecio.setBounds(405, 8, 86, 20);
+			contentPanel.add(txtPrecio);
+			txtPrecio.setColumns(10);
+		}
+		{
+			btnAgregar = new JButton("AGREGAR");
+			btnAgregar.addActionListener(this);
+			btnAgregar.setBounds(387, 53, 104, 23);
+			contentPanel.add(btnAgregar);
+		}
+		{
+			btnGenerar = new JButton("GENERAR");
+			btnGenerar.addActionListener(this);
+			btnGenerar.setBounds(387, 165, 104, 23);
+			contentPanel.add(btnGenerar);
+		}
+		{
+			btnCerrar = new JButton("CERRAR");
+			btnCerrar.addActionListener(this);
+			btnCerrar.setBounds(387, 199, 104, 23);
+			contentPanel.add(btnCerrar);
+		}
+		{
+			btnModificar =new JButton("MODIFICAR");
+			btnModificar.addActionListener(this);
+			btnModificar.setBounds(387, 87, 104, 23);
+			contentPanel.add(btnModificar);
+		}
+		{
+			scrollPane = new JScrollPane();
+			scrollPane.setBounds(10, 53, 367, 169);
+			contentPanel.add(scrollPane);
+			{
+				tS = new JTable();
+				tS.setModel(new DefaultTableModel(
+					new Object[][] {
+					},
+					new String[] {
+						"Producto", "Cantidad", "Precio"
+					}
+				));
+				tS.getColumnModel().getColumn(0).setPreferredWidth(125);
+				tS.getColumnModel().getColumn(1).setPreferredWidth(55);
+				scrollPane.setViewportView(tS);
+			}
+		}
+		{
+			btnQuitar = new JButton("QUITAR");
+			btnQuitar.addActionListener(this);
+			btnQuitar.setBounds(387, 121, 104, 23);
+			contentPanel.add(btnQuitar);
+		}
+	}
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnCerrar) {
+			do_btnCerrar_actionPerformed(e);
+		}
+		if (e.getSource() == btnGenerar) {
+			do_btnGenerar_actionPerformed(e);
+		}
+		if (e.getSource() == btnQuitar) {
+			do_btnQuitar_actionPerformed(e);
+		}
+		if (e.getSource() == btnModificar) {
+			do_btnModificar_actionPerformed(e);
+		}
+		if (e.getSource() == cbProducto) {
+			do_cbProducto_1_actionPerformed(e);
+		}
+		if (e.getSource() == btnAgregar) {
+			do_btnAgregar_actionPerformed(e);
+		}
+	}
+	
+	protected void do_cbProducto_1_actionPerformed(ActionEvent e) {
+		CalcularPrecio();
+	}
+	
+	protected void do_btnAgregar_actionPerformed(ActionEvent e) {
+		
+		String nombre = PosicionProducto().getProducto();
+		int cantidad = CantidadProducto();
+		double precio = Double.parseDouble(txtPrecio.getText());
+		boolean repetido = false;
+		
+		DefaultTableModel modelo = (DefaultTableModel) tS.getModel();
+		
+		if (modelo.getRowCount() > 0) {
+			for (int i = 0; i < modelo.getRowCount(); i++) {
+		        String nombreTabla = modelo.getValueAt(i, 0).toString();
+		        if (nombreTabla.equals(nombre)) {
+
+		            int cantidadTotal = Integer.parseInt(modelo.getValueAt(i, 1).toString()) + CantidadProducto();
+		            double precioTotal = Double.parseDouble(modelo.getValueAt(i, 2).toString()) + Double.parseDouble(txtPrecio.getText());
+
+		            modelo.setValueAt(cantidadTotal, i, 1);
+		            modelo.setValueAt(String.format("%.2f", precioTotal), i, 2);
+		            repetido = true;
+		            break;
+		        }
+			}
+			if (repetido == false) modelo.addRow(new Object[] {nombre, cantidad, precio});
+		} else modelo.addRow(new Object[] {nombre, cantidad, precio});
+		
+		RestaurarPosicion();
+	}
+	
+	protected void do_btnModificar_actionPerformed(ActionEvent e) {
+		String producto = cbProducto.getSelectedItem().toString();
+	    int nuevaCantidad = CantidadProducto();
+	    DefaultTableModel model = (DefaultTableModel) tS.getModel();
+	    boolean encontrado = false;
+
+	    for (int i = 0; i < model.getRowCount(); i++) {
+	        if (model.getValueAt(i, 0).toString().equals(producto)) {
+	            double precioUnitario = PosicionProducto().getPrecio();
+	            double nuevoPrecio = nuevaCantidad * precioUnitario;
+
+	            model.setValueAt(nuevaCantidad, i, 1);
+	            model.setValueAt(String.format("%.2f", nuevoPrecio), i, 2);
+
+	            encontrado = true;
+	            break;
+	        }
+	    }
+
+	    if (!encontrado) {
+	        JOptionPane.showMessageDialog(this, 
+	            "El producto no está en la lista", 
+	            "Error", 
+	            JOptionPane.ERROR_MESSAGE);
+	    }
+
+	    RestaurarPosicion();
+	}
+	
+	protected void do_btnQuitar_actionPerformed(ActionEvent e) {
+		String producto = cbProducto.getSelectedItem().toString();
+		 DefaultTableModel model = (DefaultTableModel) tS.getModel();
+		 boolean existe = false;
+		 for (int i = 0; i < model.getRowCount(); i++) {
+		     if (model.getValueAt(i, 0).toString().equals(producto)) {
+		         model.removeRow(i);
+		         existe = true;
+		         break;
+		     }
+		 }
+		 if (!existe) {
+		     JOptionPane.showMessageDialog(this, 
+		         "El producto no está en la lista", 
+		         "Error", 
+		         JOptionPane.ERROR_MESSAGE);
+		 }
+		 RestaurarPosicion();
+	}
+	
+	protected void do_btnGenerar_actionPerformed(ActionEvent e) {
+		DefaultTableModel model = (DefaultTableModel) tS.getModel();
+	    StringBuilder boleta = new StringBuilder();
+	    double total = 0;
+
+	    boleta.append("Boleta de Compras\n");
+	    boleta.append("--------------------\n");
+
+	    for (int i = 0; i < model.getRowCount(); i++) {
+	        String producto = model.getValueAt(i, 0).toString();
+	        int cantidad = Integer.parseInt(model.getValueAt(i, 1).toString());
+	        double precio = Double.parseDouble(model.getValueAt(i, 2).toString());
+	        total += precio;
+
+	        boleta.append(String.format("%s - Cantidad: %d - Precio: %.2f\n", producto, cantidad, precio));
+	    }
+
+	    boleta.append("--------------------\n");
+	    boleta.append(String.format("Total: %.2f", total));
+
+	    JOptionPane.showMessageDialog(this, boleta.toString(), "Boleta de Compras", JOptionPane.INFORMATION_MESSAGE);
+
+	    model.setRowCount(0);
+	    
+	    RestaurarPosicion();
+	}
+	
+	protected void do_btnCerrar_actionPerformed(ActionEvent e) {
+		dispose();
+	}
+	
+	public Producto PosicionProducto() {
+		ArregloProducto pro = new ArregloProducto();
+		return pro.Obtener(cbProducto.getSelectedIndex());
+	}
+	
+	public int CantidadProducto() {
+		return Integer.parseInt(sCantidad.getValue().toString());
+	}
+	
+	public void CalcularPrecio(int x) {
+		ArregloProducto pro = new ArregloProducto();
+		int cantidadProducto = Integer.parseInt(sCantidad.getValue().toString());
+		double total = cantidadProducto * pro.Obtener(x).getPrecio();
+		txtPrecio.setText(String.format("%.2f", total));
+	}
+	
+	public void CalcularPrecio() {
+		CalcularPrecio(cbProducto.getSelectedIndex());
+	}
+	
+	public void RestaurarPosicion() {
+		cbProducto.setSelectedIndex(0);
+	    sCantidad.setValue(1);
+	}
+}
