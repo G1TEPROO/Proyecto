bdd1b407b08b73d446dcfd3c0dfdfb03bbd10f26
G1TEPROO <mlprimero@hotmail.com>
2025-06-26 12:32:47 -0500

    empleado actualizado

--- Proyecto/src/clases/Empleado.java
+++ Proyecto/src/clases/Empleado.java
@@ -1,7 +1,7 @@
 package clases;
 
 public class Empleado {
-	private String codigo;
+    private String codigo;
     private String nombre;
     private String apellido;
     private String dni;
@@ -17,23 +17,49 @@ public class Empleado {
         this.sueldo = sueldo;
     }
 
-    public String getCodigo() { return codigo; }
-    public void setCodigo(String codigo) { this.codigo = codigo; }
+    public String getCodigo() {
+        return codigo;
+    }
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getNombre() {
+        return nombre;
+    }
 
-    public String getApellido() { return apellido; }
-    public void setApellido(String apellido) { this.apellido = apellido; }
+    public String getApellido() {
+        return apellido;
+    }
 
-    public String getDni() { return dni; }
-    public void setDni(String dni) { this.dni = dni; }
+    public String getDni() {
+        return dni;
+    }
 
-    public String getCargo() { return cargo; }
-    public void setCargo(String cargo) { this.cargo = cargo; }
+    public String getCargo() {
+        return cargo;
+    }
 
-    public double getSueldo() { return sueldo; }
-    public void setSueldo(double sueldo) { this.sueldo = sueldo; }
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
 
     public String toString() {
         return codigo + " - " + nombre + " " + apellido + " - DNI: " + dni + " - " + cargo + " - S/ " + sueldo;
