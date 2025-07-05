00aaa6f6ef4ac87b763975c3bf7780f73cea2c4f
sebastian <sebastian@DESKTOP-7RJD46D>
2025-07-05 00:20:55 -0500

    a

--- Proyecto/src/gui/LogIn.java
+++ Proyecto/src/gui/LogIn.java
@@ -18,6 +18,7 @@ import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
 
 public class LogIn extends JDialog implements ActionListener {
 
@@ -30,43 +31,50 @@ public class LogIn extends JDialog implements ActionListener {
 	private boolean logueado = false;
 	private String codigoEmpleadoLog;
 	private JButton btnIngresar;
+	private JLabel lblNewLabel_2;
 
 	/**
 	 * Create the dialog.
 	 */
 	public LogIn() {
 		setModal(true);
-		setBounds(100, 100, 290, 230);
+		setBounds(100, 100, 523, 393);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		{
 			lblNewLabel = new JLabel("Usuario:");
-			lblNewLabel.setBounds(22, 90, 83, 14);
+			lblNewLabel.setBounds(108, 155, 83, 14);
 			contentPanel.add(lblNewLabel);
 		}
 		{
 			lblNewLabel_1 = new JLabel("Contraseña:");
-			lblNewLabel_1.setBounds(22, 124, 83, 14);
+			lblNewLabel_1.setBounds(108, 196, 83, 14);
 			contentPanel.add(lblNewLabel_1);
 		}
 		{
 			txtUsuario = new JTextField();
-			txtUsuario.setBounds(99, 87, 149, 20);
+			txtUsuario.setBounds(181, 152, 161, 20);
 			contentPanel.add(txtUsuario);
 			txtUsuario.setColumns(10);
 		}
 		{
 			pswContra = new JPasswordField();
-			pswContra.setBounds(98, 121, 150, 20);
+			pswContra.setBounds(181, 193, 162, 20);
 			contentPanel.add(pswContra);
 		}
 		{
 			btnIngresar = new JButton("ingresar");
 			btnIngresar.addActionListener(this);
-			btnIngresar.setBounds(84, 157, 89, 23);
+			btnIngresar.setBounds(219, 224, 95, 32);
 			contentPanel.add(btnIngresar);
+		}
+		{
+			lblNewLabel_2 = new JLabel("");
+			lblNewLabel_2.setIcon(new ImageIcon("C:\\Users\\sebastian\\Desktop\\newrpoyecto\\Proyecto\\Proyecto\\src\\imagenes\\loginfondo.png"));
+			lblNewLabel_2.setBounds(0, 0, 512, 361);
+			contentPanel.add(lblNewLabel_2);
 		}
 	}
 	public void actionPerformed(ActionEvent e) {
